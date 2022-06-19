d24198624950d68bac4adf07de887c1af089048b
Mathilde <lavielle33.mathilde@gmail.com>
2022-06-19 23:32:07 +0200

    Modification

--- Calculatrice/Calculatrice.py
+++ Calculatrice/Calculatrice.py
@@ -1,3 +1,17 @@
-a = input("Entrez le premier nombre : ")
-b = input ("Entrez un deuxième nombre : ")
-print (f"Le résultat de l'adition {a} avec {b} est égal à {int(a) + int(b)}")
+# Variables
+a = b = ""
+
+# Boucle, tant qu'a et b ne sont pas des nombres
+while not (a.isdigit() and b.isdigit()):
+
+    # Récupération des saisies de l'utilisateur
+    a = input("Entrez le premier nombre : ")
+    b = input("Entrez le deuxième nombre : ")
+
+    # Affichage d'erreur saisie autre que des nombres
+    if not (a.isdigit() and b.isdigit()):
+        print("Veuillez saisir des nombres valides")
+
+# Affichage résultat de l'addition
+print(f"Le résultat de l'addition de {a} avec {b} est égal à {int(a) + int(b)}")
+
