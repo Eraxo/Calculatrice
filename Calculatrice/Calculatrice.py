b01c7c82f90d461f3b8e869009bea23f8f87e6b7
Mathilde <lavielle33.mathilde@gmail.com>
2022-06-19 23:08:13 +0200

    Modification

--- Calculatrice/Calculatrice.py
+++ Calculatrice/Calculatrice.py
@@ -1,3 +1,3 @@
-a = input("Entrez un nombre : ")
+a = input("Entrez le premier nombre : ")
 b = input ("Entrez un deuxième nombre : ")
 print (f"Le résultat de l'adition {a} avec {b} est égal à {int(a) + int(b)}")
